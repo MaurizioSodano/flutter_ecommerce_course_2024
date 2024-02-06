cfc5dbe36c0d2a8ffd8b62aa757d4838d87245f4
Maurizio Sodano <maurizio.sodano@gmail.com>
2024-02-06 11:04:31 +0100

    refactored AuthRepository

---
+++ ecommerce_app/lib/src/features/authentication/data/auth_repository.dart
@@ -0,0 +1,9 @@
+import '../domain/app_user.dart';
+
+abstract class AuthRepository {
+  Stream<AppUser?> authStateChanges();
+  AppUser? get currentUser;
+  Future<void> signInWithEmailAndPassword(String email, String password);
+  Future<void> createUserWithEmailAndPassword(String email, String password);
+  Future<void> signOut();
+}
--- ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
+++ ecommerce_app/lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,44 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../domain/app_user.dart';
-
-abstract class AuthRepository {
-  Stream<AppUser?> authStateChanges();
-  AppUser? get currentUser;
-  Future<void> signInWithEmailAndPassword(String email, String password);
-  Future<void> createUserWithEmailAndPassword(String email, String password);
-  Future<void> signOut();
-}
-
-class FirebaseAuthRepository implements AuthRepository {
-  @override
-  Stream<AppUser?> authStateChanges() {
-    // TODO: implement authStateChanges
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> createUserWithEmailAndPassword(String email, String password) {
-    // TODO: implement createUserWithEmailAndPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  // TODO: implement currentUser
-  AppUser? get currentUser => throw UnimplementedError();
-
-  @override
-  Future<void> signInWithEmailAndPassword(String email, String password) {
-    // TODO: implement signInWithEmailAndPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
-  }
-}
+import 'auth_repository.dart';
 
 class FakeAuthRepository implements AuthRepository {
   @override
