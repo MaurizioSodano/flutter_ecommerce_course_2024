c98bd13a5be340080336248322812220f1998993
Maurizio Sodano <maurizio.sodano@gmail.com>
2024-02-07 10:51:54 +0100

    auth signout flow

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen.dart
@@ -1,5 +1,4 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
-import 'package:ecommerce_app/src/features/authentication/data/auth_repository.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:flutter/material.dart';
@@ -9,35 +8,57 @@ import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import 'account_screen_controller.dart';
+
 /// Simple account screen showing some user info and a logout button.
 class AccountScreen extends ConsumerWidget {
   const AccountScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen<AsyncValue<void>>(accountScreenControllerProvider,
+        (previous, state) {
+      if (!state.isLoading && state.hasError) {
+        showExceptionAlertDialog(
+          context: context,
+          title: "Error".hardcoded,
+          exception: state.error,
+        );
+      }
+    });
+
+    final state = ref.watch(accountScreenControllerProvider);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account'.hardcoded),
+        title: state.isLoading
+            ? const CircularProgressIndicator()
+            : Text('Account'.hardcoded),
         actions: [
           ActionTextButton(
             text: 'Logout'.hardcoded,
-            onPressed: () async {
-              // * Get the navigator beforehand to prevent this warning:
-              // * Don't use 'BuildContext's across async gaps.
-              // * More info here: https://youtu.be/bzWaMpD1LHY
-              final goRouter = GoRouter.of(context);
-              final logout = await showAlertDialog(
-                context: context,
-                title: 'Are you sure?'.hardcoded,
-                cancelActionText: 'Cancel'.hardcoded,
-                defaultActionText: 'Logout'.hardcoded,
-              );
-              if (logout == true) {
-                await ref.read(authRepositoryProvider).signOut();
-                // TODO: only pop on success
-                goRouter.pop();
-              }
-            },
+            onPressed: state.isLoading
+                ? null
+                : () async {
+                    // * Get the navigator beforehand to prevent this warning:
+                    // * Don't use 'BuildContext's across async gaps.
+                    // * More info here: https://youtu.be/bzWaMpD1LHY
+                    final goRouter = GoRouter.of(context);
+                    final logout = await showAlertDialog(
+                      context: context,
+                      title: 'Are you sure?'.hardcoded,
+                      cancelActionText: 'Cancel'.hardcoded,
+                      defaultActionText: 'Logout'.hardcoded,
+                    );
+                    if (logout == true) {
+                      final success = await ref
+                          .read(accountScreenControllerProvider.notifier)
+                          .signOut();
+
+                      if (success) {
+                        goRouter.pop();
+                      }
+                    }
+                  },
           ),
         ],
       ),
--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -7,20 +7,25 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
       : super(const AsyncValue<void>.data(null));
 
   final AuthRepository authRepository;
-  Future<void> signOut() async {
-    try {
-      // set state to loading
-      state = const AsyncValue<void>.loading();
+  Future<bool> signOut() async {
+    // try {
+    //   // set state to loading
+    //   state = const AsyncValue<void>.loading();
 
-      //sign out, using AuthRepository
-      await authRepository.signOut();
+    //   //sign out, using AuthRepository
+    //   await authRepository.signOut();
 
-      //if success, set state to data
-      state = const AsyncValue<void>.data(null);
-    } catch (e, st) {
-      //if error, set state to error
-      state = AsyncValue<void>.error(e, st);
-    }
+    //   //if success, set state to data
+    //   state = const AsyncValue<void>.data(null);
+    //   return true;
+    // } catch (e, st) {
+    //   //if error, set state to error
+    //   state = AsyncValue<void>.error(e, st);
+    //   return false;
+    // }
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard(() => authRepository.signOut());
+    return state.hasError == false;
   }
 }
 
