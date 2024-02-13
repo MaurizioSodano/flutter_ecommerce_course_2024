56137582e4044610d989b365d0049ff91fb00c95
Maurizio Sodano <maurizio.sodano@gmail.com>
2024-02-13 10:32:46 +0100

    mock failure

---
+++ ecommerce_app/test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -0,0 +1,55 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements FakeAuthRepository {}
+
+void main() {
+  group('AccountScreenController', () {
+    test('initial state is AsyncValue.data', () {
+      final authRepository = MockAuthRepository();
+      final controller =
+          AccountScreenController(authRepository: authRepository);
+      verifyNever(authRepository.signOut);
+      expect(controller.state, const AsyncData<void>(null));
+    });
+    test('signOut success', () async {
+      // setup
+      final authRepository = MockAuthRepository();
+      //method stub
+      when(authRepository.signOut).thenAnswer(
+        (_) => Future.value(),
+      );
+      final controller = AccountScreenController(
+        authRepository: authRepository,
+      );
+      // run
+      await controller.signOut();
+      // verify
+      verify(authRepository.signOut).called(1);
+      expect(controller.state, const AsyncData<void>(null));
+    });
+
+    test('signOut failure', () async {
+      // setup
+      final authRepository = MockAuthRepository();
+      //method stub
+      final exception = Exception("Connection failed");
+      when(authRepository.signOut).thenThrow(exception);
+      final controller = AccountScreenController(
+        authRepository: authRepository,
+      );
+      // run
+      await controller.signOut();
+      // verify
+      verify(authRepository.signOut).called(1);
+
+      // just check if the state has an error
+      expect(controller.state.hasError, true);
+      // or use a type matcher without checking the value
+      expect(controller.state, isA<AsyncError>());
+    });
+  });
+}
