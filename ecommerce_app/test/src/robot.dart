e04add004ef6d2233ed0799de479f2c9abbc8196
Maurizio Sodano <maurizio.sodano@gmail.com>
2024-02-16 14:30:55 +0100

    completed auth flow test

---
+++ ecommerce_app/test/src/robot.dart
@@ -0,0 +1,61 @@
+import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/authentication/data/auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:ecommerce_app/src/features/products/presentation/home_app_bar/more_menu_button.dart';
+import 'package:ecommerce_app/src/features/products/presentation/products_list/product_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router/go_router.dart';
+
+import 'features/authentication/auth_robot.dart';
+
+class Robot {
+  Robot(this.tester) : auth = AuthRobot(tester);
+  final WidgetTester tester;
+  final AuthRobot auth;
+
+  Future<void> pumpMyApp({FakeAuthRepository? authRepository}) async {
+    // ensure URL changes in the address bar when using push / pushNamed
+    // more info here: https://docs.google.com/document/d/1VCuB85D5kYxPR3qYOjVmw8boAGKb7k62heFyfFHTOvw/edit
+    GoRouter.optionURLReflectsImperativeAPIs = true;
+    final authRepository = FakeAuthRepository(addDelay: false);
+    final productsRepository = FakeProductsRepository(addDelay: false);
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          authRepositoryProvider.overrideWithValue(authRepository),
+          productsRepositoryProvider.overrideWithValue(productsRepository),
+        ],
+        child: MaterialApp.router(
+          routerConfig: GoRouter(initialLocation: '/', routes: [
+            GoRoute(
+              path: '/',
+              builder: (context, state) => const MyApp(),
+            ),
+          ]),
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+  }
+
+  void expectFindAllProductCards() {
+    final finder = find.byType(ProductCard);
+    expect(finder, findsNWidgets(kTestProducts.length));
+  }
+
+  Future<void> openPopupMenu() async {
+    final finder = find.byType(MoreMenuButton);
+    final matches = finder.evaluate();
+    // if an item is found, it means that we're running on a small window
+    // and can tap to reveal the menu
+    if (matches.isNotEmpty) {
+      await tester.tap(finder);
+      await tester.pumpAndSettle();
+    }
+    // else no-op, as the items are already visible
+  }
+}
