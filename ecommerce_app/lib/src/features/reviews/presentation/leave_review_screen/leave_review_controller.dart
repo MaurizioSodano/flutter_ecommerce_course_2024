9eb1f040b8d86b6b732e317904754a5d3f4782d1
Maurizio Sodano <maurizio.sodano@gmail.com>
2024-02-24 12:49:20 +0100

    Testing the LeaveReviewForm and preventing an AssertionError

---
+++ ecommerce_app/lib/src/features/reviews/presentation/leave_review_screen/leave_review_controller.dart
@@ -0,0 +1,41 @@
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:ecommerce_app/src/features/reviews/application/reviews_service.dart';
+import 'package:ecommerce_app/src/features/reviews/data/fake_reviews_repository.dart';
+import 'package:ecommerce_app/src/features/reviews/domain/review.dart';
+import 'package:ecommerce_app/src/utils/current_date_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class LeaveReviewController extends StateNotifier<AsyncValue<void>> {
+  LeaveReviewController(
+      {required this.reviewsService, required this.currentDateBuilder})
+      : super(const AsyncData(null));
+
+  final ReviewsService reviewsService;
+  final DateTime Function() currentDateBuilder;
+
+  Future<void> submitReview({
+    required ProductID productId,
+    required double rating,
+    required String comment,
+  }) async {
+    final review = Review(
+      rating: rating,
+      comment: comment,
+      date: currentDateBuilder(),
+    );
+    state = const AsyncLoading();
+    final newState = await AsyncValue.guard(() =>
+        reviewsService.submitReview(productId: productId, review: review));
+    if (mounted) {
+      state = newState;
+    }
+  }
+}
+
+final leaveReviewControllerProvider =
+    StateNotifierProvider.autoDispose<LeaveReviewController, AsyncValue<void>>(
+        (ref) {
+  return LeaveReviewController(
+      reviewsService: ref.watch(reviewsServiceProvider),
+      currentDateBuilder: ref.watch(currentDateBuilderProvider));
+});
--- ecommerce_app/lib/src/features/reviews/presentation/leave_review_screen/leave_review_screen.dart
+++ ecommerce_app/lib/src/features/reviews/presentation/leave_review_screen/leave_review_screen.dart
@@ -1,7 +1,9 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
 import 'package:ecommerce_app/src/constants/breakpoints.dart';
+import 'package:ecommerce_app/src/features/reviews/presentation/leave_review_screen/leave_review_controller.dart';
 import 'package:ecommerce_app/src/features/reviews/presentation/product_reviews/product_rating_bar.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:ecommerce_app/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
 import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
@@ -17,7 +19,6 @@ class LeaveReviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Read from data source
     const review = null;
     return Scaffold(
       appBar: AppBar(
@@ -64,6 +65,9 @@ class _LeaveReviewFormState extends ConsumerState<LeaveReviewForm> {
 
   @override
   Widget build(BuildContext context) {
+    ref.listen<AsyncValue>(leaveReviewControllerProvider,
+        (_, state) => state.showAlertDialogOnError(context));
+    final state = ref.watch(leaveReviewControllerProvider);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -94,12 +98,15 @@ class _LeaveReviewFormState extends ConsumerState<LeaveReviewForm> {
         gapH32,
         PrimaryButton(
           text: 'Submit'.hardcoded,
-          // TODO: Loading state
-          isLoading: false,
-          onPressed: _rating == 0
+          isLoading: state.isLoading,
+          onPressed: state.isLoading || _rating == 0
               ? null
-              // TODO: submit review
-              : () => showNotImplementedAlertDialog(context: context),
+              : () =>
+                  ref.read(leaveReviewControllerProvider.notifier).submitReview(
+                        productId: widget.productId,
+                        rating: _rating,
+                        comment: _controller.text,
+                      ),
         )
       ],
     );
