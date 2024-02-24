4116d368ca678396be83dbf6a67f25fd98be16ee
Maurizio Sodano <maurizio.sodano@gmail.com>
2024-02-24 19:10:13 +0100

    Converting the AccountScreenController to use Riverpod Generator

--- ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,22 +1,41 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:async';
+
 import 'package:ecommerce_app/src/features/authentication/data/auth_repository.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue<void>> {
-  AccountScreenController({required this.authRepository})
-      : super(const AsyncData(null));
+part 'account_screen_controller.g.dart';
 
-  final AuthRepository authRepository;
+@riverpod
+class AccountScreenController extends _$AccountScreenController {
+  @override
+  FutureOr<void> build() {
+    //nothing to do
+  }
   Future<void> signOut() async {
+    final authRepository = ref.read(authRepositoryProvider);
     state = const AsyncLoading();
     state = await AsyncValue.guard(() => authRepository.signOut());
   }
 }
 
-final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
-    AccountScreenController, AsyncValue<void>>((ref) {
-  final authRepository = ref.watch(authRepositoryProvider);
-  return AccountScreenController(
-    authRepository: authRepository,
-  );
-});
+
+
+// class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+//   AccountScreenController({required this.authRepository})
+//       : super(const AsyncData(null));
+
+//   final AuthRepository authRepository;
+//   Future<void> signOut() async {
+//     state = const AsyncLoading();
+//     state = await AsyncValue.guard(() => authRepository.signOut());
+//   }
+// }
+
+// final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
+//     AccountScreenController, AsyncValue<void>>((ref) {
+//   final authRepository = ref.watch(authRepositoryProvider);
+//   return AccountScreenController(
+//     authRepository: authRepository,
+//   );
+// });
