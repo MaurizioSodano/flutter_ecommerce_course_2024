0983925fde47b7403084dc0026f5de0a968144bf
Maurizio Sodano <maurizio.sodano@gmail.com>
2024-02-25 11:10:36 +0100

    updated tests with riverpod 2

--- ecommerce_app/test/src/features/purchase_flow_test.dart
+++ ecommerce_app/test/src/features/purchase_flow_test.dart
@@ -4,6 +4,9 @@ import '../robot.dart';
 
 void main() {
   testWidgets('Full purchase flow', (tester) async {
+    // * Note: All tests are wrapped with `runAsync` to prevent this error:
+    // * A Timer is still pending even after the widget tree was disposed.
+    await tester.runAsync(() async {
     final r = Robot(tester);
     await r.pumpMyApp();
     r.products.expectFindAllProductCards();
@@ -37,5 +40,6 @@ void main() {
     await r.auth.tapLogoutButton();
     await r.auth.tapDialogLogoutButton();
     r.products.expectFindAllProductCards();
+  });
   });
 }
